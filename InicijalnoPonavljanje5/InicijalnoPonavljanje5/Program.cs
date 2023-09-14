ad31e873aa81ac93282ca783ca7676f8f05885a8
Andrej <75538726+AndrejBuraz@users.noreply.github.com>
2023-09-14 19:53:01 +0200

    Add files via upload

---
+++ InicijalnoPonavljanje5/InicijalnoPonavljanje5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InicijalnoPonavljanje5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Pojednostavljena provjera ispravnosti URL adrese registrirane u hrvatskom internetskom prostoru bila bi:
+                • mora počinjati s www
+                • mora završavati s hr
+                • mora sadržavati točku nakon www i prije hr
+                Napišite program koji će zatražiti od korisnika da unese URL adresu, te će provjeriti je li adresa napisana u ispravnom formatu.
+            */
+            Console.WriteLine("Unesi hipervezu: ");
+            string link = Console.ReadLine();
+            if(link[0] == 'w' && link[1] == 'w' && link[2] == 'w' && link[3] == '.' && link[link.Length-1] == 'r' && link[link.Length - 2] == 'h' && link[link.Length - 3] == '.')
+            {
+                Console.WriteLine("Hiperveza poštuje propisani format!");
+            }
+            else
+            {
+                Console.WriteLine("Hiperveza NE poštuje propisani format!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
